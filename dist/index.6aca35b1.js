d38523ee61d80642215ef5a21c4da48bf8d4bd41
Flavio Irala <flavioiralap@gmail.com>
2024-11-11 11:48:12 -0400

    build corigida novamente kk

---
+++ dist/index.6aca35b1.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Dec 03,2024 19:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime(),o=Math.floor(n/864e5),a=Math.floor(n%864e5/36e5),l=Math.floor(n%36e5/6e4),r=Math.floor(n%6e4/1e3);document.getElementById("contador").innerHTML=`${o}d ${a}h ${l}m ${r}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML=`est\xe1 vazio! Alguem j\xe1 pegou :(`)},1e3);
+//# sourceMappingURL=index.6aca35b1.js.map
